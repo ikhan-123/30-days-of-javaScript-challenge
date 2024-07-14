c19e47b43602f153e5436e6a9cc214cf66802b6a
ikhan-123 <ifficodder@gmail.com>
2024-07-14 13:38:28 +0500

    task 1 to task 5 complete

---
+++ 02-day/index.js
@@ -0,0 +1,23 @@
+console.log("Day-02");
+// Day 2 Operators
+
+// Arthmetic Operators
+// Task 1:
+let num = 10 + 10;
+console.log(num);
+// Task 2:
+let subract = 10 - 10
+console.log(subract);
+// Task 3:
+let multiply = 18 * 18
+console.log(multiply);
+// Task 4:
+let divide = 34 / 2
+console.log(divide);
+// Task 5:
+let modulous = 20 % 8
+console.log(modulous);
+
+
+//  Activity Assignment Operators
+//  Task 6:
