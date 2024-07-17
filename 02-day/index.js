500acc5907bcdaf57ca7a59d0b0553387e72bd0a
ikhan-123 <ifficodder@gmail.com>
2024-07-17 15:34:29 +0500

    "Removed console logs for arithmetic operators, added code for assignment operators, comparison operators, and conditional statement"

--- 02-day/index.js
+++ 02-day/index.js
@@ -1,23 +1,75 @@
-console.log("Day-02");
-// Day 2 Operators
+// console.log("Day-02");
+// // Day 2 Operators
 
-// Arthmetic Operators
-// Task 1:
-let num = 10 + 10;
-console.log(num);
-// Task 2:
-let subract = 10 - 10
-console.log(subract);
-// Task 3:
-let multiply = 18 * 18
-console.log(multiply);
-// Task 4:
-let divide = 34 / 2
-console.log(divide);
-// Task 5:
-let modulous = 20 % 8
-console.log(modulous);
+// // Arthmetic Operators
+// // Task 1:
+// let num = 10 + 10;
+// console.log(num);
+// // Task 2:
+// let subract = 10 - 10
+// console.log(subract);
+// // Task 3:
+// let multiply = 18 * 18
+// console.log(multiply);
+// // Task 4:
+// let divide = 34 / 2
+// console.log(divide);
+// // Task 5:
+// let modulous = 20 % 8
+// console.log(modulous);
 
 
-//  Activity Assignment Operators
-//  Task 6:
+// //  Activity 2: Assignment Operators
+// //  Task 6:
+// let a = 10
+// let b = 15
+// a += b
+// console.log(a);
+
+// // Task 7:
+// let c = 10
+// let d = 15
+// c -= d
+// console.log(c);
+
+
+// // Activity 3: Camparison Operators
+// // Task 8:
+// let num1 = 2 > 5;
+// console.log(num1);
+// num1 = 10 < 12
+// console.log(num1);
+// // Task 9:
+// let num2 = 10 >= 12;
+// console.log(num2);
+// let num3 = 13 <= 12;
+// console.log(num3);
+// // Task 10:
+// let num4 = 10 === 10;
+// console.log(typeof num4);
+
+// // Activity 4: Logical Operators
+// // Task 11:
+// let age = 22;
+// if (age >= 18 && age <= 22) {
+//      console.log("you are eligible");
+// }
+// //  Task 12:
+// let a = 10
+// let b = 12
+// if (a >= 10 || b <= 12) {
+//      console.log("you are eligible");
+// }
+//  Task 13:
+let A = 10
+if(!A >= 12){
+     console.log("you are not eligible");
+}
+//  Activity 5: Ternary operators
+//  Task 14:
+let z = 100;
+let y = 50;
+y = (z == 100)? "true" : "false";
+console.log(y);
+
+
